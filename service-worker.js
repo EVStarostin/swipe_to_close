eadb28a3aebc2a085e4c5cfd5bbd1576f6b21652
evstarostin89 <evstarostin89@yandex-team.ru>
2019-05-22 12:32:33 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/swipe_to_close/precache-manifest.560408f16492e4c8a53391f7a1b43a22.js"
+  "/swipe_to_close/precache-manifest.c523f454ec66be7744c58855170d5d0f.js"
 );
 
 self.addEventListener('message', (event) => {
